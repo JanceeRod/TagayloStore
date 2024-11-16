789abf2c14131f5459c596f42bec1601b60740ae
Jancee <jancee800@gmail.com>
2024-11-16 16:18:20 +0800

    move to operations

--- src/mainPackage/Operations.java
+++ src/mainPackage/Operations.java
@@ -1,7 +1,11 @@
 package mainPackage;
 
 import E_Package.Category;
+import G_Package.customRoundedPanel;
 
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.io.*;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -10,6 +14,40 @@ import java.util.Map;
 
 public class Operations extends Definitions {
 
+    public static JPanel createCustomPanel(int width, int height, Object backgroundSource, LayoutManager layout) {
+        JPanel customPanel = new JPanel();
+        customPanel.setPreferredSize(new Dimension(width, height));
+        if (backgroundSource instanceof JPanel) {
+            customPanel.setBackground(((JPanel) backgroundSource).getBackground());
+        } else if (backgroundSource instanceof Color) {
+            customPanel.setBackground((Color) backgroundSource);
+        }
+        customPanel.setLayout(new BorderLayout());
+        return customPanel;
+    }
+
+    public static JLabel createCustomLabel(String text, Color foreground, Font font, int x, int y, int width, int height, int topBorder, int leftBorder, int bottomBorder, int rightBorder, int horizontalAlignment) {
+        JLabel label = new JLabel();
+        label.setText(text);
+        label.setForeground(foreground);
+        label.setFont(font);
+        label.setBounds(x, y, width, height);
+        label.setBorder(new EmptyBorder(topBorder, leftBorder, bottomBorder, rightBorder));
+        label.setHorizontalAlignment(horizontalAlignment);
+        return label;
+    }
+
+    public static customRoundedPanel createCustomRoundedPanel(int radius, int borderTop, int borderLeft, int borderBottom, int borderRight, Object backgroundSource, LayoutManager layout) {
+        customRoundedPanel customPanel = new customRoundedPanel(radius);
+        customPanel.setBorder(BorderFactory.createEmptyBorder(borderTop, borderLeft, borderBottom, borderRight));
+        if (backgroundSource instanceof JPanel) {
+            customPanel.setBackground(((JPanel) backgroundSource).getBackground());
+        } else if (backgroundSource instanceof Color) {
+            customPanel.setBackground((Color) backgroundSource);
+        }
+        customPanel.setLayout(layout);
+        return customPanel;
+    }
 
     //utility method
     public static String toTitleCase(String input) {
